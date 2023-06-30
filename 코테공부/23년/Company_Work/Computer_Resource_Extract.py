f2cf3afb25f75ad37f8d3a526365892ea9feff32
태현의 저장소 <47412931+tae-hyunkim@users.noreply.github.com>
2023-06-30 21:19:03 +0900

    Computing Resource Check - 미완
    추가작업필요

---
+++ 코테공부/23년/Company_Work/Computer_Resource_Extract.py
@@ -0,0 +1,18 @@
+# 윈도우 노트북에서 CPU/RAM/GPU Memory Check 
+
+# H/W Spec 
+## CPU : i5-8250-U
+## RAM : 16GB (8GB * 2EA)
+## GPU : Geforce GTX 1050
+
+# GPU 사용량을 알기위해 Nvidia Drive, CUDA, Cudnn 작업을 진행하였으나 GPU 인식이 안됨
+## 추가 작업 진행하기로 함 (23.07.01)
+### visual studio 설치부터 하자.
+
+
+import psutil # CPU, Memory Resource Extract 
+from tensorflow.python.client import device_lib
+
+print('Using CPU Percent : {}'.format(psutil.cpu_percent()))
+print('Using RAM Percent : {}'.format(psutil.virtual_memory().percent))
+print(device_lib.list_local_devices())
