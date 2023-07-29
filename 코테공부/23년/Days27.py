cef2ac0311f9715e674ed4209d514da17b8dc451
태현의 저장소 <47412931+tae-hyunkim@users.noreply.github.com>
2023-07-29 21:05:08 +0900

    Days 27
    2일에 걸쳐서 문제 해결
    
    sorted 해서 가장 많은 에너지를 쓰는경우부터 삭제하는 방식으로 진행해야 하나, 노가다 코드로 짯었음 -> 재미는 있는 문제

--- 코테공부/23년/Days27.py
+++ 코테공부/23년/Days27.py
@@ -1,31 +1,70 @@
 # 프로그래머스 코딩테스트 연습 Level 2 / 광물 캐기
-
+import math
 def solution(picks, minerals):
     answer = 0
-    pirodo = [[1,1,1],[5,1,1],[25,5,1]]
+    case = []
+    gok_cnt = sum(picks)
+    for i in range(math.ceil(len(minerals)/5)):
+        if i >= gok_cnt:
+            break
+        if len(minerals) > 4:
+            chk = minerals[:5]
+        else:
+            chk = minerals
+        case.append([chk,chk.count('diamond'),chk.count('iron')]) # 5개씩 광물캐는 Case Define
+        minerals = minerals[5:]
 
-    while minerals :
-        if 'diamond' in minerals:
-            if 'diamond' in minerals[:4]:
-                if picks[0] > 0:
-                    pick = 0
-                    picks[0] = picks[0] - 1 
-                elif picks[1] > 0 :
-                    pick = 1
-                    picks[1] = picks[1] - 1
-                else  :
-                    pick = 2
-                    picks[2] = picks[2] - 1
-            
-            elif 'iron' in minerals[:4]:
-                if picks[1] > 0:
-                    pick = 0
-                    picks[1] = picks[1] - 1 
-                elif picks[0] > 0 :
-                    pick = 1
-                    picks[0] = picks[0] - 1
-                else  :
-                    pick = 2
-                    picks[2] = picks[2] - 1
-            
+    # 방법을 다르게 가서 정렬을 진행한다 : 다이아 몬드 > 철 많은순 > 나머지  
+    case = sorted(case, key = lambda x: (-x[1], -x[2]))
+    dia_gok_chk = picks[0]
+    iron_gok_chk = picks[1]
+    dia_iron_gok_chk = dia_gok_chk + iron_gok_chk
+    for i,check in enumerate(case) :
+        chk = check[0] # 광물 리스트 
+
+        if i < dia_gok_chk:
+            answer += len(chk)
+        elif i < dia_iron_gok_chk:
+            for gas in chk :
+                if gas == 'diamond':
+                    answer += 5 
+                else:
+                    answer += 1 
+        else: 
+            for gas in chk :
+                if gas == 'diamond':
+                    answer += 25 
+                elif gas == 'iron':
+                    answer += 5
+                else:
+                    answer += 1
     return answer
+
+# print("Test 1 : {}".format(solution([1,3,2],["diamond", "diamond",
+#                                               "diamond", "iron", "iron", 
+#                                               "diamond", "iron", "stone"])))
+
+# print("Test 2 : {}".format(solution([0,1,1],["diamond", "diamond", "diamond", 
+#                                              "diamond", "diamond", "iron", 
+#                                              "iron", "iron", "iron", 
+#                                              "iron", "diamond"])))
+
+# print("Test 3 : {}".format(solution([1,1,0],["stone", "stone", "iron", 
+#                                              "stone", "diamond", "diamond",
+#                                             "diamond", "diamond", "diamond", 
+#                                             "diamond"])))
+# print("Test 4 : {}".format(solution([1,0,0],["stone", "stone", "iron", 
+#                                              "stone", "diamond", "diamond",
+#                                             "diamond", "diamond", "diamond", 
+#                                             "diamond"])))
+# print("Test 5 : {}".format(solution([1,1,1],["stone", "stone", "iron",
+#                                              "iron", "iron", "iron", "iron"])))
+# print("Test 6 : {}".format(solution([1,1,1],["stone", "stone", "stone", "stone", "stone", 
+#                                              "diamond", "diamond", "diamond", "diamond", "diamond",
+#                                               "stone", "stone", "stone", "stone", "stone"])))
+# print("Test 7 : {}".format(solution([1,1,1],["diamond", "diamond", "diamond", "diamond", "diamond", 
+#                                              "iron", "iron", "iron", "iron", "iron",
+#                                              "diamond", "diamond", "diamond", "diamond", "diamond"])))
+
+print("Test 8 : {}".format(solution([1,0,1],["iron", "iron", "iron", "iron",
+                                              "diamond", "diamond", "diamond"])))
