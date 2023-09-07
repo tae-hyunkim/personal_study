5d3a3f71ed31aba3554eab68a65fcb1ccbd36a28
태현의 저장소 <47412931+tae-hyunkim@users.noreply.github.com>
2023-09-07 21:34:30 +0900

    days48

---
+++ 코테공부/23년/Days48.py
@@ -0,0 +1,16 @@
+# 프로그래머스 코딩테스트 연습 Level 2 / 택배 상자
+from collections import deque
+
+def solution(order):
+    answer = 0
+    lst = []
+    chk = 0
+    for i in range(1,len(order) + 1):
+        lst.append(i)
+        while lst and lst[-1] == order[chk] :
+            lst.pop()
+            answer += 1
+            chk += 1
+    return answer
+
+print("Test 1 : {}".format(solution([4,3,1,2,5])))
