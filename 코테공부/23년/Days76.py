8257284b1e569d4ff761928a15156342dcc3d26d
태현의 저장소 <47412931+tae-hyunkim@users.noreply.github.com>
2024-02-03 17:02:57 +0900

    Days76

---
+++ 코테공부/23년/Days76.py
@@ -0,0 +1,31 @@
+from itertools import combinations
+
+def solution(relation):
+    row = len(relation)
+    col = len(relation[0])
+
+    #가능한 속성의 모든 인덱스 조합 
+    combi = []
+    for i in range(1, col+1):
+        combi.extend(combinations(range(col), i))
+        
+    #유일성
+    unique = []
+    for i in combi:
+        tmp = [tuple([item[key] for key in i]) for item in relation]
+
+        # 모든 Row의 unique 수가 Row수와 같을 때 
+        if len(set(tmp)) == row:    # 유일성
+            put = True
+            
+            for x in unique:
+                if set(x).issubset(set(i)):  # 최소성
+                    put = False
+                    break
+                    
+            if put: 
+                unique.append(i)
+   
+    return len(unique)
+
+print("Test 1 : {}".format(solution([["100","ryan","music","2"],["200","apeach","math","2"],["300","tube","computer","3"],["400","con","computer","4"],["500","muzi","music","3"],["600","apeach","music","2"]])))
